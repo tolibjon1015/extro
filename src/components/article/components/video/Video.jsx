63dc7d1eac0f3ea2071f73b16d3e76409ca9af8a
tolibjon1015 <tolibboyjamoliddinov45@gmail.com>
2023-03-29 21:19:09 +0500

    complete tab

--- src/components/article/components/video/Video.jsx
+++ src/components/article/components/video/Video.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import "./video.css"
+import data from '../../data/video'
 
 function Video() {
     return (
-        <div>Video</div>
+        <div className='border'>
+            {
+                data.map((item) => {
+                    return <div key={item.id}>
+                        <div>{item.iframe}</div>
+                    </div>
+                })
+            }
+        </div>
     )
 }
 
--- src/components/article/data/video.js
+++ src/components/article/data/video.js
@@ -1,1 +1,64 @@
+const data = [
+  {
+    id: 1,
+    iframe: (
+      <iframe
+        src="https://www.youtube.com/embed/__N4-D2aoMQ"
+        title="Payg`ambar oshig`i Hasan filmi | Пайғамбар ошиғи Ҳасан (2021)"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    id: 2,
+    iframe: (
+      <iframe
+        src="https://www.youtube.com/embed/co08RN9wtpw?list=PLys356tU5j5SHDFV3hsXo_Q8HXAD8EiNa"
+        title="Muqaddima | Anbiyolar qissasi | Ramazon 2009-yil 1-kun"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    id: 3,
+    iframe: (
+      <iframe
+        src="https://www.youtube.com/embed/T7wAyHZZvMY"
+        title="NАMАNGАN SАFАRI 06.04.2021.  (to&#39;liq)"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    id: 4,
+    iframe: (
+      <iframe
+        src="https://www.youtube.com/embed/dG_zJalnjQI"
+        title="Биз умматларгагина ҳос бўлган 5 та хушхабар!"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+  {
+    id: 5,
+    iframe: (
+      <iframe
+        src="https://www.youtube.com/embed/pybuKLwvGyk"
+        title="БАРДОШИНГИЗ ЕТСА КУРИНГ! БУ БУЛГАН ВОҚЕА"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    ),
+  },
+];
 
+export default data;
